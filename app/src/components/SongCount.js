4b3a2e74b97852979b35403b87c0908f56910258
jake matillano <jakematillano.z@gmail.com>
2023-11-02 21:38:48 -0700

    adjusting column sizes on table as well as logic changes to time display so it doesn't display time units of 0

--- app/src/components/SongCount.js
+++ app/src/components/SongCount.js
@@ -3,15 +3,24 @@ import Pagination from './Pagination';
 
 const SongCountTable = ({ data, currentPage, itemsPerPage, onPageChange, totalPages }) => {
 
-  const formatTime = (ms) => {
-    const secs = Math.floor(ms/1000)
-    const days = Math.floor(secs / (3600 * 24))
-    const hrs = Math.floor((secs % (3600 * 24)) / 3600)
-    const mins = Math.floor((secs % 3600) / 60)
-    const remainingSecs = secs % 60
+  function formatTime(ms) {
+    const seconds = Math.floor(ms / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
 
-    return `${days} days, ${hrs} hours, ${mins} minutes, ${remainingSecs} seconds`
-  };
+    const formattedTime = [];
+
+    if (days > 0) formattedTime.push(`${days} days`);
+
+    if (hours % 24 > 0) formattedTime.push(`${hours % 24} hrs`);
+
+    if (minutes % 60 > 0) formattedTime.push(`${minutes % 60} mins`);
+
+    if (seconds % 60 > 0) formattedTime.push(`${seconds % 60} secs`);
+
+    return formattedTime.join(', ');
+  }
 
   const songCounts = {};
   const songTimes = {};
@@ -37,6 +46,11 @@ const SongCountTable = ({ data, currentPage, itemsPerPage, onPageChange, totalPa
     <div className="container">
       <h2>Songs by Count</h2>
       <table>
+        <colgroup>
+          <col style={{ width: '65%' }} />
+          <col style={{ width: '10%' }} />
+          <col style={{ width: '25%' }} />
+        </colgroup>
         <thead>
           <tr>
             <th>Song Title</th>
