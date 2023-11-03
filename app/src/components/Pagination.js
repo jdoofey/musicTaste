6644c0f29e89632b93158dfefe819f9fefd4024d
jake matillano <jakematillano.z@gmail.com>
2023-11-02 19:41:24 -0700

    creating pagination component to make table prettier

---
+++ app/src/components/Pagination.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Pagination = ({currentPage, totalPages, onPageChange}) => {
+  const pageNumbers = [...Array(totalPages).keys()].map((num) => num + 1);
+
+  return (
+    <div>
+      <ul className="pagination">
+        <li
+          className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}
+          onClick={() => onPageChange(currentPage - 1)}
+        >
+          <button className="page-link">Previous</button>
+        </li>
+        {pageNumbers.map((page) => (
+          <li
+            key={page}
+            className={`page-item ${currentPage === page ? 'active' : ''}`}
+            onClick={() => onPageChange(page)}
+          >
+            <button className="page-link">{page}</button>
+          </li>
+        ))}
+        <li
+          className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}
+          onClick={() => onPageChange(currentPage + 1)}
+        >
+          <button className="page-link">Next</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Pagination;
+
